fce2532cc40c02bd1d136c0bae373f2ced45b817
giakiet4-7 <ngkiet2411@clc.fitus.edu.vn>
2026-08-01 22:43:56 +0700

    Merge Auth và Test

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    host: 'localhost', // Hoặc '0.0.0.0' nếu muốn cho phép truy cập từ các thiết bị khác trong cùng mạng LAN
+    port: 5173,        // Thay đổi port theo ý muốn (mặc định của Vite thường là 5173)
+    strictPort: true,  // Nếu port 3000 bị chiếm, Vite sẽ báo lỗi thay vì tự động đổi sang port khác
+    cors: true,        // Bật CORS nếu cần gọi API từ backend khác domain
+  }
 })
